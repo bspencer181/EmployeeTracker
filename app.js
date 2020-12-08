92a035bea483d6d8b3915c0028a8c159c5d7b549
Beth Spencer <beth.spencer@student.labette.edu>
2020-12-08 16:48:16 -0600

    added role and dept function

---
+++ app.js
@@ -0,0 +1,116 @@
+//Dependencies
+
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const table = require('console.table');
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  port     : 3306,
+  user     : 'root',
+  password : 'password',
+  database : 'employee_tracker'
+});
+ 
+//Connection
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end(); 
+
+//Initial prompt
+function initialPrompt(){
+inquirer.prompt({
+    name: "start",
+    message: "What would you like to do?",
+    type: "list",
+    choices: ["Add Deparments","Add Roles","Add Employees","View Departments",'View Roles', 'View Emploees', 'Update Employee Roles']
+}).then(function(response){
+    switch (response) {
+        case "Add Departments":
+                addDept();
+            break;  
+        case "Add Roles":
+                addRoles();
+            break;
+        case "Add Employee":
+                addEmployee();
+            break;
+        case "View Departments":
+                viewDepartments();
+            break;        
+        case "View Roles":
+                viewRoles();
+            break;       
+         case "viewEmployees":
+                viewEmployees();
+            break;       
+        case "Update Employee Roles":
+                updateRole();
+            break;
+    }
+})
+}
+
+// Add Dept
+function addDept (){
+    inquirer.prompt([
+        {
+            name: "dept",
+            type: "input",
+            message: "What department would you like to add?"
+        }
+    ])
+    .then(function(res){
+        var query = connection.query(
+            "INSERT INTO department SET ? ",
+            {
+                name: res.name
+            },
+            function(err){
+                if (err) throw err
+                console.table(res);
+                initialPrompt();
+            }
+        )
+    })
+}
+
+// Add Role
+function addRoles(){
+    connection.query("SELECT role.title AS Title, role.salary AS Salary FROM role", function(err,res){
+        inquirer.prompt([
+            {
+                name: "Title",
+                type: "input",
+                message: "What is their title?"
+            },
+
+            {
+                name: "Salary",
+                type:"input",
+                message:"What is their salary?"
+            }
+        ])
+        .then(function(res){
+            connection.query(
+                "INSERT INTO role SET ?",
+                {
+                    title: res.Title,
+                    salary: res.Salary,
+                },
+                function(err){
+                    if (err) throw err
+                    console.table(res);
+                    initialPrompt();
+                }
+            )
+        });
+
+    });
+}
